a6163f4d78685426eeb7e384393bfe921fbe1324
Max Chegwyn <98077619+maxchegwyn@users.noreply.github.com>
2026-03-26 12:40:07 +0400

    Clean up diagnostic script

--- scripts/diagnose_token.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-with open('/tmp/mic_token.json', 'rb') as f:
-    content = f.read()
-
-print('Length:', len(content))
-print('Bytes 860-880:', content[860:880])
-print('Last 10 bytes:', content[-10:])
