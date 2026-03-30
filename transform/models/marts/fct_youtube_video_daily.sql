87c1e6055c1f8ac40bc6ddd977ec6c1d2ca2e236
Max Chegwyn <98077619+maxchegwyn@users.noreply.github.com>
2026-03-30 16:43:32 +0400

    rename fct_youtube_calendar to fct_youtube_video_daily; add fct_youtube_channel_daily with subscribers_net

---
+++ transform/models/marts/fct_youtube_video_daily.sql
@@ -0,0 +1,15 @@
+select
+    s.date,
+    s.video_id,
+    v.content_type,
+    v.exercise_category,
+    s.watch_hours,
+    s.views,
+    s.estimated_revenue,
+    s.subscribers_gained,
+    s.likes,
+    s.comments,
+    s.shares
+from {{ ref('stg_youtube') }} s
+left join {{ ref('dim_videos') }} v
+    on s.video_id = v.video_id
