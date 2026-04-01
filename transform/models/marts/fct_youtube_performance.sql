f6a54cd796e3d9f0e8f24cf7654359657c158544
Max Chegwyn <98077619+maxchegwyn@users.noreply.github.com>
2026-04-01 13:27:16 +0400

    fix: replace 91.25-day quarter logic with calendar-month boundaries

--- transform/models/marts/fct_youtube_performance.sql
+++ transform/models/marts/fct_youtube_performance.sql
@@ -28,10 +28,48 @@ with_quarter as (
         d.estimated_revenue,
         d.subscribers_gained,
         v.publish_date,
-        cast(
-            ceil(date_diff(d.date, v.publish_date, day) / 91.25)
-            as int64
-        )                                       as quarter_number
+        case
+            when d.date >= v.publish_date
+             and d.date <  date_add(v.publish_date, interval 3  month) then 1
+            when d.date >= date_add(v.publish_date, interval 3  month)
+             and d.date <  date_add(v.publish_date, interval 6  month) then 2
+            when d.date >= date_add(v.publish_date, interval 6  month)
+             and d.date <  date_add(v.publish_date, interval 9  month) then 3
+            when d.date >= date_add(v.publish_date, interval 9  month)
+             and d.date <  date_add(v.publish_date, interval 12 month) then 4
+            when d.date >= date_add(v.publish_date, interval 12 month)
+             and d.date <  date_add(v.publish_date, interval 15 month) then 5
+            when d.date >= date_add(v.publish_date, interval 15 month)
+             and d.date <  date_add(v.publish_date, interval 18 month) then 6
+            when d.date >= date_add(v.publish_date, interval 18 month)
+             and d.date <  date_add(v.publish_date, interval 21 month) then 7
+            when d.date >= date_add(v.publish_date, interval 21 month)
+             and d.date <  date_add(v.publish_date, interval 24 month) then 8
+            when d.date >= date_add(v.publish_date, interval 24 month)
+             and d.date <  date_add(v.publish_date, interval 27 month) then 9
+            when d.date >= date_add(v.publish_date, interval 27 month)
+             and d.date <  date_add(v.publish_date, interval 30 month) then 10
+            when d.date >= date_add(v.publish_date, interval 30 month)
+             and d.date <  date_add(v.publish_date, interval 33 month) then 11
+            when d.date >= date_add(v.publish_date, interval 33 month)
+             and d.date <  date_add(v.publish_date, interval 36 month) then 12
+            when d.date >= date_add(v.publish_date, interval 36 month)
+             and d.date <  date_add(v.publish_date, interval 39 month) then 13
+            when d.date >= date_add(v.publish_date, interval 39 month)
+             and d.date <  date_add(v.publish_date, interval 42 month) then 14
+            when d.date >= date_add(v.publish_date, interval 42 month)
+             and d.date <  date_add(v.publish_date, interval 45 month) then 15
+            when d.date >= date_add(v.publish_date, interval 45 month)
+             and d.date <  date_add(v.publish_date, interval 48 month) then 16
+            when d.date >= date_add(v.publish_date, interval 48 month)
+             and d.date <  date_add(v.publish_date, interval 51 month) then 17
+            when d.date >= date_add(v.publish_date, interval 51 month)
+             and d.date <  date_add(v.publish_date, interval 54 month) then 18
+            when d.date >= date_add(v.publish_date, interval 54 month)
+             and d.date <  date_add(v.publish_date, interval 57 month) then 19
+            when d.date >= date_add(v.publish_date, interval 57 month)
+             and d.date <  date_add(v.publish_date, interval 60 month) then 20
+        end as quarter_number
     from daily as d
     inner join videos as v
         on d.video_id = v.video_id
@@ -49,6 +87,7 @@ aggregated as (
         min(date)                               as quarter_start_date,
         max(date)                               as quarter_end_date
     from with_quarter
+    where quarter_number is not null
     group by video_id, quarter_number
 )
 
